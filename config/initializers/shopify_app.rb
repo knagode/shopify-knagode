bbbd5ee6da69f69fe45e4141335103268569822a
Klemen Nagode <klemen.nagode@gmail.com>
2017-03-08 17:47:58 +0100

    app proxy

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -4,4 +4,12 @@ ShopifyApp.configure do |config|
   config.secret = "7a0d9add0c05aad22267091da7a37792"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
+  # config.webhooks = [
+  #     {topic: 'carts/update', address: 'https://example-app.com/webhooks/carts_update'},
+  #     {topic: 'products/update', address: 'https://example-app.com/webhooks/products_update', fields: ['title', 'vendor']}
+  #   ]
+  config.scripttags = [
+      {event:'onload', src: 'https://shopify-knagode.herokuapp.com/fancy.js'}
+      #{event:'onload', src: ->(domain) { dynamic_tag_url(domain) } }
+    ]
 end
